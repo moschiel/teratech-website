aedfcb7a494ecf45e1cfc65daa65881978b2eebb
unknown <moschiel@hotmail.com>
2021-04-11 00:48:57 -0300

    applied slidedown animation to menu using jquery

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,30 +1,47 @@
 import React from 'react'; 
-
-import menuIcon from './menu.png';
-
+import $ from 'jquery';
+import menuIconImg from './menu.png';
 import "./Navigation.css";
+
+//eventos do icone de menu
+function handleMenuIconClick () {
+    $(".mobile-menu").slideToggle();
+}
+function handleMenuIconOnMouseEnter () {
+    $(".mobile-menu").slideDown();
+}
+//eventos para todo o menu inteiro
+function handleNavOnMouseLeave () {
+    $(".mobile-menu").slideUp();
+}
 
 function Navigation () {
     return (
-        <div className="navigation">
+        <div className="navigation"  onMouseLeave={handleNavOnMouseLeave}>
             <nav class="desk-menu">
-            <ul>
-                <li><a href="index.html">home</a></li>
-                <li class="autoscroll"><a href="#service">serviços</a></li>
-                <li><a href="./criacao-de-sites.html">WebSite</a></li>
-                <li><a href="./criacao-de-robos.html">Robotize</a></li>
-            </ul>
+                <ul>
+                    <li><a href="index.html">home</a></li>
+                    <li class="autoscroll"><a href="#service">serviços</a></li>
+                    <li><a href="./criacao-de-sites.html">WebSite</a></li>
+                    <li><a href="./criacao-de-robos.html">Robotize</a></li>
+                </ul>
             </nav>
     
-            <img class="menu" src={menuIcon} alt="menu" />
+            <img 
+                class="menu" 
+                src={menuIconImg} 
+                alt="menu" 
+                onClick={handleMenuIconClick} 
+                onMouseEnter={handleMenuIconOnMouseEnter}
+            />
             
-            <nav class="mobile-menu">
-            <ul>
-                <li><a href="#home">home</a></li>
-                <li class="autoscroll"><a href="#service">serviços</a></li>
-                <li><a href="./criacao-de-sites.html">WebSite</a></li>
-                <li><a href="./criacao-de-robos.html">Robotize</a></li>
-            </ul>
+            <nav class="mobile-menu" >
+                <ul>
+                    <li><a href="#home">home</a></li>
+                    <li class="autoscroll"><a href="#service">serviços</a></li>
+                    <li><a href="./criacao-de-sites.html">WebSite</a></li>
+                    <li><a href="./criacao-de-robos.html">Robotize</a></li>
+                </ul>
             </nav>
         </div>
     );

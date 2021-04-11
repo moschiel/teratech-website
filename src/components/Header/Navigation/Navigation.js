66224866f06eaae7b44e720944684572d824ab4b
unknown <moschiel@hotmail.com>
2021-04-11 20:32:36 -0300

    removido duplicidade de menus do html, a alteração do modo do menu agora é feito via script

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -3,30 +3,44 @@ import $ from 'jquery';
 import menuIconImg from './menu.png';
 import "./Navigation.css";
 
-//eventos do icone de menu
+//evento de clique do menu, que executa o slideToogle
 function handleMenuIconClick () {
     $(".mobile-menu").slideToggle();
 }
+//evento acionado quando mouse entra no icone do menu-mobile, que executa o slideDown
 function handleMenuIconOnMouseEnter () {
     $(".mobile-menu").slideDown();
 }
-//eventos para todo o menu inteiro
+//evento acionado quando o mouse sai da elemento '.navigation', que executa o slideUp
 function handleNavOnMouseLeave () {
     $(".mobile-menu").slideUp();
 }
 
+//troca o modo do menu, de acordo com o tamanho atual da tela
+function setMenuMode() {
+    if($(window).width() < 768){
+        $('#nav-menu').removeClass('desk-menu');
+        $('#nav-menu').addClass('mobile-menu');
+    }else {
+        $('#nav-menu').removeClass('mobile-menu');
+        $('#nav-menu').addClass('desk-menu');
+    }
+}
+//evento acionado quando a tela muda de tamanho 
+$(window).resize(() => {
+    setMenuMode();
+});
+
+
+
 function Navigation () {
+    //useEffect executa depois do elemento ser renderizado na tela
+    React.useEffect( () => {
+        setMenuMode();
+    });
+
     return (
         <div className="navigation"  onMouseLeave={handleNavOnMouseLeave}>
-            <nav className="desk-menu">
-                <ul>
-                    <li><a href="/">home</a></li>
-                    <li className="autoscroll"><a href="#service">serviços</a></li>
-                    <li><a href="/criacao-de-sites">WebSite</a></li>
-                    <li><a href="/criacao-de-robos">Robotize</a></li>
-                </ul>
-            </nav>
-    
             <img 
                 className="menu" 
                 src={menuIconImg} 
@@ -34,8 +48,8 @@ function Navigation () {
                 onClick={handleMenuIconClick} 
                 onMouseEnter={handleMenuIconOnMouseEnter}
             />
-            
-            <nav className="mobile-menu" >
+
+            <nav id="nav-menu">
                 <ul>
                     <li><a href="/">home</a></li>
                     <li className="autoscroll"><a href="#service">serviços</a></li>
